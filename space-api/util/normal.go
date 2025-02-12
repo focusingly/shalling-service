37156928cbde94bfe7c9397795a6e2e7346072a9
shalling <oops@shalling.me>
2025-02-12 10:46:31 +0000

    添加流包装类

--- space-api/util/normal.go
+++ space-api/util/normal.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"io"
 	"iter"
 )
 
@@ -11,6 +12,7 @@ func GetSerial[T any](seed func() T, optionLimit ...int) iter.Seq[T] {
 	return func(yield func(T) bool) {
 		switch {
 		case len(optionLimit) == 0:
+			// A infinity producer
 			for yield(seed()) {
 			}
 		case len(optionLimit) == 1:
@@ -28,5 +30,30 @@ func GetSerial[T any](seed func() T, optionLimit ...int) iter.Seq[T] {
 		default:
 			panic(fmt.Sprintf("require one arg, but got %d", len(optionLimit)))
 		}
+	}
+}
+
+type writeCounterWrapper struct {
+	writer io.Writer
+	count  int64
+}
+
+var _ io.Writer = (*writeCounterWrapper)(nil)
+
+func (bc *writeCounterWrapper) Write(p []byte) (n int, err error) {
+	n, err = bc.writer.Write(p)
+	bc.count += int64(n)
+	return
+}
+
+// NewByteWriteCountWrapper 对一个写入流进行包装, 返回一个额外的字节写入统计函数
+func NewByteWriteCountWrapper(writer io.Writer) (wrapperWriter io.Writer, writeCounter func() int64) {
+	wr := &writeCounterWrapper{
+		writer: writer,
+		count:  0,
+	}
+
+	return wr, func() int64 {
+		return wr.count
 	}
 }

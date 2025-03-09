70cc93ddf1854555e1740d2d1aa4e2c5704c2547
Shalling <hi-shalling@users.noreply.github.com>
2025-03-09 16:47:07 +0000

    提取响应处理

--- space-api/internal/controller/admin/blocking_ip.controller.go
+++ space-api/internal/controller/admin/blocking_ip.controller.go
@@ -11,58 +11,78 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UseBlockingIPController(group *gin.RouterGroup) {
-	blockingGroup := group.Group("/ip")
-	blockingService := blocking.DefaultIPBlockingService
+type blockingController struct {
+	blockingService blocking.INetBlockingService
+}
 
-	// 获取 IP 黑名单列表
-	blockingGroup.GET("/list", func(ctx *gin.Context) {
+func (s *blockingController) listBLockingList() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		req := &dto.GetBlockingPagesReq{}
 		if err := ctx.ShouldBindQuery(req); err != nil {
 			ctx.Error(util.CreateBizErr("参数错误: "+err.Error(), err))
 			return
 		}
-		if resp, err := blockingService.GetBlockingPages(req, ctx); err != nil {
+		if resp, err := s.blockingService.GetBlockingPages(req, ctx); err != nil {
 			ctx.Error(err)
 		} else {
 			outbound.NotifyProduceResponse(resp, ctx)
 		}
-	})
+	}
+}
 
-	// 删除黑名单中的 IP 记录
-	blockingGroup.POST("/delete", func(ctx *gin.Context) {
+func (s *blockingController) deleteBlockingRecords() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		req := &dto.DeleteBlockingRecordReq{}
 		if err := ctx.ShouldBindQuery(req); err != nil {
 			ctx.Error(util.CreateBizErr("参数错误: "+err.Error(), err))
 			return
 		}
-		if resp, err := blockingService.DeleteBlockingRecord(req, ctx); err != nil {
+		if resp, err := s.blockingService.DeleteBlockingRecord(req, ctx); err != nil {
 			ctx.Error(err)
 		} else {
 			outbound.NotifyProduceResponse(resp, ctx)
 		}
-	})
+	}
+}
 
-	// 添加 IP 黑名单记录
-	blockingGroup.POST("/update", func(ctx *gin.Context) {
+func (s *blockingController) updateBlockingRecord() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		req := &dto.AddBlockingIPReq{}
 		if err := ctx.ShouldBindQuery(req); err != nil {
 			ctx.Error(util.CreateBizErr("参数错误: "+err.Error(), err))
 			return
 		}
-		if resp, err := blockingService.AddBlockingIP(req, ctx); err != nil {
+		if resp, err := s.blockingService.AddBlockingIP(req, ctx); err != nil {
 			ctx.Error(err)
 		} else {
 			outbound.NotifyProduceResponse(resp, ctx)
 		}
-	})
+	}
+}
 
-	// 同步数据
-	blockingGroup.POST("/sync", func(ctx *gin.Context) {
-		if err := blockingService.SyncBlockingRecordInCache(context.TODO()); err != nil {
+func (s *blockingController) syncBlockingRecords() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if err := s.blockingService.SyncBlockingRecordInCache(context.TODO()); err != nil {
 			ctx.Error(err)
 		} else {
 			outbound.NotifyProduceResponse(&performance.Empty{}, ctx)
 		}
-	})
+	}
+}
+
+func UseBlockingIPController(group *gin.RouterGroup) {
+	blockingGroup := group.Group("/ip")
+	blockingService := blocking.DefaultIPBlockingService
+	controller := &blockingController{
+		blockingService: blockingService,
+	}
+
+	// 获取 IP 黑名单列表
+	blockingGroup.GET("/list", controller.listBLockingList())
+	// 删除黑名单中的 IP 记录
+	blockingGroup.POST("/delete", controller.deleteBlockingRecords())
+	// 添加 IP 黑名单记录
+	blockingGroup.POST("/update", controller.updateBlockingRecord())
+	// 同步数据
+	blockingGroup.POST("/sync", controller.syncBlockingRecords())
 }
